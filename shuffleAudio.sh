5ed9c33e4eadb52c7936ac2adb7973949d5d0a69
Moulik24 <moulikapmehta@gmail.com>
2022-08-24 12:14:57 -0500

    Added optional command line argument --audio, as well as a shell script that can run main.py with this argument. Added tests for get_all_sounds when have None or default type for the root_dir.

---
+++ shuffleAudio.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+audioRootDir=.
+python main.py --audio $audioRootDir
--- test_main.py
+++ test_main.py
@@ -4,6 +4,19 @@ from main import get_all_sounds
 
 
 class Test(TestCase):
-    def test_get_all_sounds(self):
+    def test_get_all_sounds_from_test_directory(self):
         all_sounds = get_all_sounds('test_sound_files')
         self.assertTrue(len(all_sounds) == 10)
+
+    def test_get_all_sounds_from_current_directory(self):
+        try:
+            all_sounds = get_all_sounds(".")
+        except:
+            assert False
+
+    def test_get_all_sounds_no_directory_specified(self):
+        try:
+            all_sounds = get_all_sounds(None)
+        except:
+            assert False
+
