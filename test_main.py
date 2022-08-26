3d511aef1af672f4ec6a32dc57e20d578172cffb
Moulik24 <moulikapmehta@gmail.com>
2022-08-26 13:50:56 -0500

    We have log_success_files.txt which contains the files that will be shuffled (could be converted to AudioSegments), and have functionality to write to it.

--- test_main.py
+++ test_main.py
@@ -22,24 +22,35 @@ class Test(TestCase):
 
     def test_convert_files_to_audio_segments_happy_path(self):
         all_audio_files = ["test_sound_files/Clubs/3_C.m4a", "test_sound_files/Spades/5_S.m4a", "test_sound_files/Hearts/5_H.m4a"]
-        shuffled_audio, error_files = convert_files_to_audio_segments(all_audio_files)
+        shuffled_audio, success_files, error_files = convert_files_to_audio_segments(all_audio_files)
         self.assertTrue(len(shuffled_audio) == 3)
-        self.assertTrue(len(error_files) == 0)
+        self.assertTrue(success_files == all_audio_files)
+        self.assertTrue(error_files == [])
 
     def test_convert_files_to_audio_segments_non_existent_but_valid_sound_file(self):
         non_existent_sound_files = ["file_that_does_not_exist.wav"]
-        shuffled_audio, error_files = convert_files_to_audio_segments(non_existent_sound_files)
+        shuffled_audio, success_files, error_files = convert_files_to_audio_segments(non_existent_sound_files)
         self.assertTrue(len(shuffled_audio) == 0)
+        self.assertTrue(success_files == [])
         self.assertTrue(error_files == non_existent_sound_files)
 
     def test_convert_files_to_audio_segments_non_existent_and_not_valid_sound_file(self):
         non_existent_non_sound_files = ["file_that_does_not_exist.txt"]
-        shuffled_audio, error_files = convert_files_to_audio_segments(non_existent_non_sound_files)
+        shuffled_audio, success_files, error_files = convert_files_to_audio_segments(non_existent_non_sound_files)
         self.assertTrue(len(shuffled_audio) == 0)
+        self.assertTrue(success_files == [])
         self.assertTrue(error_files == non_existent_non_sound_files)
 
     def test_convert_files_to_audio_segments_non_existent_files(self):
         non_audio_files = ["test_sound_files/NonAudioFiles/non_audio_file.txt", "test_sound_files/NonAudioFiles/non_audio_file_no_ext"]
-        shuffled_audio, error_files = convert_files_to_audio_segments(non_audio_files)
+        shuffled_audio, success_files, error_files = convert_files_to_audio_segments(non_audio_files)
         self.assertTrue(len(shuffled_audio) == 0)
+        self.assertTrue(success_files == [])
         self.assertTrue(error_files == non_audio_files)
+
+    def test_convert_files_to_audio_segments_mixed_path(self):
+        some_audio_files = ["test_sound_files/Clubs/3_C.m4a", "test_sound_files/NonAudioFiles/non_audio_file.txt"]
+        shuffled_audio, success_files, error_files = convert_files_to_audio_segments(some_audio_files)
+        self.assertTrue(len(shuffled_audio) == 1)
+        self.assertTrue(success_files == ["test_sound_files/Clubs/3_C.m4a"])
+        self.assertTrue(error_files == ["test_sound_files/NonAudioFiles/non_audio_file.txt"])
